e20ccd74755c60c1f41303ed2554c1b7f78ad9bb
Tatyana <tatyanajfautomation@gmail.com>
2026-03-26 16:11:07 -0400

    adding 2nd bearing

--- themes/my-custom-theme/includes/products.php
+++ themes/my-custom-theme/includes/products.php
@@ -389,7 +389,6 @@ function get_all_products($globals_id = 578) {
         'seo_title' => get_field('siemens_servo_motor_water_cooled_pn_1ft7084_5wf71_1mh1_seo_title', $globals_id),
         'meta_description' => get_field('siemens_servo_motor_water_cooled_pn_1ft7084_5wf71_1mh1_meta_description', $globals_id),
         'canonical_url' => get_field('siemens_servo_motor_water_cooled_pn_1ft7084_5wf71_1mh1_canonical_url', $globals_id),), 
-        /////// new 
       array(
         'name' => get_field('62042rs1_bearing_deep_groove_ball_bearing_name', $globals_id), 
         'id' =>  sanitize_title(get_field('62042rs1_bearing_deep_groove_ball_bearing_id', $globals_id)),
@@ -399,7 +398,18 @@ function get_all_products($globals_id = 578) {
         'category_url' => get_field('62042rs1_bearing_deep_groove_ball_bearing_category_url', $globals_id),
         'seo_title' => get_field('62042rs1_bearing_deep_groove_ball_bearing_seo_title', $globals_id),
         'meta_description' => get_field('62042rs1_bearing_deep_groove_ball_bearing_meta_description', $globals_id),
-        'canonical_url' => get_field('62042rs1_bearing_deep_groove_ball_bearing_canonical_url', $globals_id),), 
+        'canonical_url' => get_field('62042rs1_bearing_deep_groove_ball_bearing_canonical_url', $globals_id),),
+     /////// new 
+      array(
+        'name' => get_field('6204-2rs1-bearing__20_47_14_mm_deep_groove_ball_bearing_name', $globals_id), 
+        'id' =>  sanitize_title(get_field('6204-2rs1-bearing__20_47_14_mm_deep_groove_ball_bearing_id', $globals_id)),
+        'description' => get_field('6204-2rs1-bearing__20_47_14_mm_deep_groove_ball_bearing_description', $globals_id),
+        'image' => get_field('6204-2rs1-bearing__20_47_14_mm_deep_groove_ball_bearing_image', $globals_id), 
+        'category' => get_field('6204-2rs1-bearing__20_47_14_mm_deep_groove_ball_bearing_category', $globals_id), 
+        'category_url' => get_field('6204-2rs1-bearing__20_47_14_mm_deep_groove_ball_bearing_category_url', $globals_id),
+        'seo_title' => get_field('6204-2rs1-bearing__20_47_14_mm_deep_groove_ball_bearing_seo_title', $globals_id),
+        'meta_description' => get_field('6204-2rs1-bearing__20_47_14_mm_deep_groove_ball_bearing_meta_description', $globals_id),
+        'canonical_url' => get_field('6204-2rs1-bearing__20_47_14_mm_deep_groove_ball_bearing_canonical_url', $globals_id),),  
     );
 
     return array_filter($products, fn($p) => !empty($p['name']));
