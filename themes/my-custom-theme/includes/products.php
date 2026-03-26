57a9cb488242ca1cbab0d74c9ec9b9274dc4a2ad
Tatyana <tatyanajfautomation@gmail.com>
2026-03-26 16:53:46 -0400

    adding 2nd bearing

--- themes/my-custom-theme/includes/products.php
+++ themes/my-custom-theme/includes/products.php
@@ -399,6 +399,16 @@ function get_all_products($globals_id = 578) {
         'seo_title' => get_field('62042rs1_bearing_deep_groove_ball_bearing_seo_title', $globals_id),
         'meta_description' => get_field('62042rs1_bearing_deep_groove_ball_bearing_meta_description', $globals_id),
         'canonical_url' => get_field('62042rs1_bearing_deep_groove_ball_bearing_canonical_url', $globals_id),),
+         array(
+        'name' => get_field('w_6205_2rs1_c3_bearing_25_52_15_mm_stainless_steel_deep_groove_ball_bearing_name', $globals_id), 
+        'id' =>  sanitize_title(get_field('w_6205_2rs1_c3_bearing_25_52_15_mm_stainless_steel_deep_groove_ball_bearing_id', $globals_id)),
+        'description' => get_field('w_6205_2rs1_c3_bearing_25_52_15_mm_stainless_steel_deep_groove_ball_bearing_description', $globals_id),
+        'image' => get_field('w_6205_2rs1_c3_bearing_25_52_15_mm_stainless_steel_deep_groove_ball_bearing_image', $globals_id), 
+        'category' => get_field('w_6205_2rs1_c3_bearing_25_52_15_mm_stainless_steel_deep_groove_ball_bearing_category', $globals_id), 
+        'category_url' => get_field('w_6205_2rs1_c3_bearing_25_52_15_mm_stainless_steel_deep_groove_ball_bearing_category_url', $globals_id),
+        'seo_title' => get_field('w_6205_2rs1_c3_bearing_25_52_15_mm_stainless_steel_deep_groove_ball_bearing_seo_title', $globals_id),
+        'meta_description' => get_field('w_6205_2rs1_c3_bearing_25_52_15_mm_stainless_steel_deep_groove_ball_bearing_meta_description', $globals_id),
+        'canonical_url' => get_field('w_6205_2rs1_c3_bearing_25_52_15_mm_stainless_steel_deep_groove_ball_bearing_canonical_url', $globals_id),),
     );
 
     return array_filter($products, fn($p) => !empty($p['name']));
